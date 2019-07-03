cb3d682d2f6ef73b9a918dd252b123682a14387e
Matthew Davis <matthew.davis@just-eat.com>
2019-07-02 20:20:34 -0400

    init pointers

---
+++ pointers/wallet.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+
+	w.balance += amount
+}
+
+func (w *Wallet) Withdrawl(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
