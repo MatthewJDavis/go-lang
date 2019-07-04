fd32748776a4fb640a81f0ab9ab3fdf8ff086343
Matthew Davis <matthew.davis@just-eat.com>
2019-07-03 20:31:00 -0400

    refactor tests

--- learn-go-with-tests/maps/dictionary_test.go
+++ learn-go-with-tests/maps/dictionary_test.go
@@ -4,14 +4,31 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
 }
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if want != got {
 		t.Errorf("wanted '%s', but got '%s'", want, got)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error '%s' want '%s'", got, want)
 	}
 }
