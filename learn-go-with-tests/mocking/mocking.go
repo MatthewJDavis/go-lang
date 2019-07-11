741a00ea8f56451c406c9d02fbf7a5f49ad7475b
MatthewJDavis <matthewdavis111@gmail.com>
2019-07-10 21:28:26 -0400

    added func and interface

--- learn-go-with-tests/mocking/mocking.go
+++ learn-go-with-tests/mocking/mocking.go
@@ -6,6 +6,18 @@ import (
 	"os"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
 }
